4e8c39a978a9beaebf21a45be59bde64f4fe0cc0
lordfundarbyrd <lordfundarbyrd@gmail.com>
2023-03-15 15:40:59 -0400

    Add section requirements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+const FRAME_WIDTH = 500;
+const FRAME_HEIGHT = 500;
+const MARGINS = { top: 50, bottom: 50, left: 50, right: 50 };
+const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right;
+const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
+const PADDING = 15;
+
+const FRAME1 = d3
+  .select("#vis1")
+  .append("svg")
+  .attr("height", FRAME_HEIGHT)
+  .attr("width", FRAME_WIDTH)
+  .attr("class", "frame");
+
+const FRAME2 = d3
+  .select("#vis2")
+  .append("svg")
+  .attr("height", FRAME_HEIGHT)
+  .attr("width", FRAME_WIDTH)
+  .attr("class", "frame");
+
 d3.csv("data/NBA_Bets_Today.csv").then((data) => {
-    console.log(data);
+  console.log(data);
 });
+
