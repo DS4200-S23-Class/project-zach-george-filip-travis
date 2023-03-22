7f386a5c0dd839a60d70ad9f70670452dc1a7818
debruynt <debruyn.t@northeastern.edu>
2023-03-22 07:19:48 -0400

    Start of first vis, error with display

--- js/main.js
+++ js/main.js
@@ -51,9 +51,9 @@ function updateFilters() {
 const FRAME1 = d3
   .select("#vis1")
   .append("svg")
-  .attr("height", FRAME_HEIGHT)
-  .attr("width", FRAME_WIDTH)
-  .attr("class", "frame");
+    .attr("height", FRAME_HEIGHT)
+    .attr("width", FRAME_WIDTH)
+    .attr("class", "frame");
 
 const FRAME2 = d3
   .select("#vis2")
@@ -62,6 +62,43 @@ const FRAME2 = d3
   .attr("width", FRAME_WIDTH)
   .attr("class", "frame");
 
+let svg1 = d3
+  .select("body")
+  .append("svg")
+  .attr("height", VIS_HEIGHT)
+  .attr("width", VIS_WIDTH);
+
 d3.csv("data/NBA_Bets_Today.csv").then((data) => {
   console.log(data);
+
+  let bookies = Array.from(new Set(data.map((d) => d.site_title)));
+  let x_scale = d3
+    .scaleBand()
+    .domain(bookies)
+    .range([0, VIS_WIDTH]);
+
+  let y_scale = d3
+    .scaleLinear()
+    .domain(d3.extent(data.map((d) => + d["price_away"])))
+    .range([VIS_HEIGHT - MARGINS.top, MARGINS.top]);
+
+  let colors_1 = d3.scaleOrdinal().domain(bookies).range(d3.schemePaired);
+
+
+  // decide which variable size of circles (currently away point spread, want to change this to amount of money already spent by other users on this bet maybe?
+  // or amount of people?)
+  let circ_size_domain = d3.extent(data.map((d) => + d["point_spread_away"]));
+  // size the circles
+  let circ_size = d3.scaleSqrt().domain(circ_size_domain).range([2, 50]);
+
+  svg1.selectAll(".circ")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("class", "circ")
+    .attr("stroke", "black")
+    .attr("fill", (d) => color(d.site_title))
+    .attr("r", (d) => size(d["point_spread_away"]))
+    .attr("cx", (d) => x_scale(d.site_title))
+    .attr("cy", (d) => y_scale(d.price_away));
 });
